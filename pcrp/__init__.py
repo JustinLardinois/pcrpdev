9ab554be9dac244aa9e0d8c638853f4f9eb4f80c
Justin Lardinois <me@justinlardinois.com>
2014-11-23 22:23:11 -0800

    moved home page handler to pcrp/views.py

--- pcrp/__init__.py
+++ pcrp/__init__.py
@@ -14,25 +14,10 @@
 ###############################################################################
 # pcrp/__init__.py - a component of pCRP
 
-from google.appengine.api import users
-from google.appengine.ext import ndb
-
 from flask import Flask
-from flask import render_template
-
-from pcrp.conference import *
-from pcrp.keys import *
 
 app = Flask("pcrp")
 
-@app.route("/")
-def home_view():
-	metadata = metadata_key.get()
-	user = users.get_current_user()
-	return render_template("index.html",
-		conference_name=metadata.name,
-		registration_deadline=metadata.registration_deadline,
-		submission_deadline=metadata.submission_deadline,
-		user=user,
-		create_login_url=lambda x: users.create_login_url(x),
-		create_logout_url=lambda x: users.create_logout_url(x))
+# Yes, a circular import is necessary:
+# http://flask.pocoo.org/docs/0.10/patterns/packages/
+import pcrp.views
---
+++ pcrp/views.py
@@ -0,0 +1,42 @@
+# Copyright 2014 Justin Lardinois
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+# pcrp/views.py - a component of pCRP
+
+# In the spirit of following Flask conventions, this script contains all the
+# request handlers (known as views) and their routing decorators.
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+from flask import render_template
+
+# Yes, a circular import is necessary:
+# http://flask.pocoo.org/docs/0.10/patterns/packages/
+from pcrp import app
+
+from pcrp.conference import *
+from pcrp.keys import *
+
+@app.route("/")
+def home_view():
+	metadata = metadata_key.get()
+	user = users.get_current_user()
+	return render_template("index.html",
+		conference_name=metadata.name,
+		registration_deadline=metadata.registration_deadline,
+		submission_deadline=metadata.submission_deadline,
+		user=user,
+		create_login_url=lambda x: users.create_login_url(x),
+		create_logout_url=lambda x: users.create_logout_url(x))
