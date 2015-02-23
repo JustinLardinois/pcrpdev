7727528772bc9b9a24c69a976d61dabdd20e770d
Justin Lardinois <me@justinlardinois.com>
2015-02-22 22:31:23 -0800

    started ConflictSet class

--- pcrp/models.py
+++ pcrp/models.py
@@ -25,6 +25,28 @@ class Conference(ndb.Model):
 	registration_deadline = ndb.DateTimeProperty()
 	submission_deadline = ndb.DateTimeProperty()
 
+# Conflicts of interest need only be one-way for there to be an issue.
+# For example, while A indicating a conflict with B is different from
+# B indicating a conflict with A, either is sufficient for it to be an
+# issue.
+# This model represents indicated conflicts as a set of pairs of user IDs.
+class ConflictSet(ndb.Model):
+	conflicts = ndb.JsonProperty()
+
+	def __init__(self):
+		self.conflicts = set()
+
+	def add(self,a,b):
+		self.conflicts.add(tuple([a,b]))
+	
+	def delete(self,a,b):
+		self.conflicts.discard(tuple([a,b]))
+	
+	def is_conflict(self,a,b):
+		x = tuple([a,b])
+		y = tuple([b,a])
+		return x in self.conflicts or y in self.conflicts
+
 # class for keeping track of users, since App Engine's native Users class
 # is not reliable for long term storage and additional information is needed
 class ConferenceUser(ndb.Model):
