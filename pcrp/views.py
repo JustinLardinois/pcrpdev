00cb016afe62ef8d156d0803fa7f9187a9151672
Justin Lardinois <me@justinlardinois.com>
2015-03-08 18:50:19 -0700

    added reviewer preferences to assign page

--- pcrp/views.py
+++ pcrp/views.py
@@ -595,7 +595,9 @@ def preferences_view_get():
 	papers = Paper.query(Paper.author != user.key).fetch()
 	papers = [p for p in papers if not conflicts.is_conflict(user.id,
 		p.author.get().id)]
-	prefs = {}
+	# list of papers the user does not have a conflict with
+	
+	prefs = {} # dict of the user's review preferences
 	for p in papers:
 		key = p.key.urlsafe()
 		try:
@@ -654,6 +656,12 @@ def assign_view_get():
 	
 	user = lookup_user(users.get_current_user().user_id())
 	
+	def lookup_preference(paper,user_id): # prevent exceptions in template
+		try:
+			return paper.get_preference(user_id)
+		except KeyError:
+			return MAX_PREFERENCE // 2
+	
 	return render_template(
 		"review/assign.html",
 		conference_name=metadata.name,
@@ -665,5 +673,6 @@ def assign_view_get():
 		papers=Paper.query().fetch(),
 		reviewers=ConferenceUser.query(
 			ConferenceUser.program_committee == True).fetch(),
-		conflicts=conflict_key.get()
+		conflicts=conflict_key.get(),
+		lookup_preference=lookup_preference
 	)
